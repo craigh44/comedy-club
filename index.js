66fc228b6118c6f23d234f43b2697fd38a681c5d
Craig Horsborough <craighorsborough@gmail.com>
2016-02-02 21:41:08 +0000

    Added heroku port code

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/views/index.html');
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
 	console.log('Listening on port 3000');
 });
